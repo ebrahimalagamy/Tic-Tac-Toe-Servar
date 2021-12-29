ffde30c21e1d6a3b9a42a3ba6f8a47ca0ee7de3e
ebrahimalagamy <ebrahimalagamy@email.com>
2021-12-29 15:21:34 +0200

    Initial commit

---
+++ src/tic/tac/toe/server/TicTacToeServer.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tic.tac.toe.server;
+
+/**
+ *
+ * @author Ebrah
+ */
+public class TicTacToeServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
