443a9ebb79df595be2e43c7463c05a2f925a81d1
MohamedKamal16 <mohamedkamalkamel16@gmail.com>
2022-01-04 10:36:13 +0200

    commit 2

--- src/tic/tac/toe/server/TicTacToeServer.java
+++ src/tic/tac/toe/server/TicTacToeServer.java
@@ -1,18 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tic.tac.toe.server;
 
 import java.net.ServerSocket;
 import java.sql.DriverManager;
 import java.sql.*;
-import org.apache.derby.jdbc.ClientDriver;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,18 +18,25 @@ class DataHandle extends Thread {
     PreparedStatement pst;
     DataInputStream dis;
     DataOutputStream dos;
+    Socket s;
     
     public DataHandle(Socket s){
             try {
+                
                 try {
                     DriverManager.registerDriver( new org.apache.derby.jdbc.ClientDriver());
                      con = DriverManager.getConnection("jdbc:derby://localhost:1527/IdDbs","Mohamed16","161996");}
                 catch (SQLException ex) {Logger.getLogger(DataHandle.class.getName()).log(Level.SEVERE, null, ex);}
                 
+                this.s = s;
                 dis = new DataInputStream(s.getInputStream());
                 dos = new DataOutputStream(s.getOutputStream());
                 start();}
             catch (IOException ex) {Logger.getLogger(DataHandle.class.getName()).log(Level.SEVERE, null, ex);}
+    }
+    
+    public static enum requestTypes{
+        register,login,getData,setData,setMove
     }
     
     @Override
@@ -42,28 +44,30 @@ class DataHandle extends Thread {
         while(true){
             try {
                 String data = dis.readUTF();
+           
                 String[] arrOfStrings = data.split("\\+");
-                switch(arrOfStrings.length){
-                    case 3:
+                requestTypes Key = requestTypes.valueOf(arrOfStrings[0]);
+
+                switch(Key){
+                    case register:
                     try {
                     pst = con.prepareStatement("insert into Player(NAME,PASSWORD,EMAIL) values(?,?,?)");
-                    pst.setString(1,arrOfStrings[0]);
-                    pst.setString(2,arrOfStrings[1]);
-                    pst.setString(3,arrOfStrings[2]);
+                    pst.setString(1,arrOfStrings[1]);
+                    pst.setString(2,arrOfStrings[2]);
+                    pst.setString(3,arrOfStrings[3]);
                     pst.executeUpdate();
                     
                     } catch (SQLException ex) {
-                        Logger.getLogger(DataHandle.class.getName()).log(Level.SEVERE, null, ex);
-                    }
+                        Logger.getLogger(DataHandle.class.getName()).log(Level.SEVERE, null, ex);}
                     break;
-                    //not send
-                    case 2:
+
+                    case login:
                     try {
                     pst = con.prepareStatement("select * from Player where NAME = ?");
-                    pst.setString(1,arrOfStrings[0]);
+                    pst.setString(1,arrOfStrings[1]);
                     rs = pst.executeQuery();
                     while (rs.next()){
-                    if((rs.getString(3)).equals(arrOfStrings[1]))
+                    if((rs.getString(3)).equals(arrOfStrings[2]))
                         dos.writeUTF("validsignin");
                     else
                         dos.writeUTF("invalidsignin");                    
@@ -71,7 +75,24 @@ class DataHandle extends Thread {
                     catch (SQLException ex) {Logger.getLogger(DataHandle.class.getName()).log(Level.SEVERE, null, ex); }
                     break;
                 }
-            } catch (IOException ex) {Logger.getLogger(DataHandle.class.getName()).log(Level.SEVERE, null, ex);}
+            } 
+            catch(SocketException ex){
+                try {
+                    s.close();
+                    dis.close();
+                    dos.close();
+                    con.close();
+                    
+                } catch (IOException ex1) {
+                    Logger.getLogger(DataHandle.class.getName()).log(Level.SEVERE, null, ex1);
+                } catch (SQLException ex1) {
+                    Logger.getLogger(DataHandle.class.getName()).log(Level.SEVERE, null, ex1);
+                } finally{
+                   System.out.println("user Closed");
+                   break; 
+                }
+            }
+            catch (IOException ex) {Logger.getLogger(DataHandle.class.getName()).log(Level.SEVERE, null, ex);}
         }
            
     }
@@ -89,12 +110,6 @@ Connection con;
 
         public TicTacToeServer() throws IOException
         {
-                try {
-                    DriverManager.registerDriver(new ClientDriver());
-                    con=DriverManager.getConnection("jdbc:derby://localhost:1527/IdDbs","Mohamed16","161996");}
-                catch (SQLException ex) { Logger.getLogger(TicTacToeServer.class.getName()).log(Level.SEVERE, null, ex);}
-                
-                
             serverSocket = new ServerSocket(6060);
             while(true){
                 Socket s = serverSocket.accept();
